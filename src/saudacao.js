346cdfcafe7fb4842bdd152917955a0b9ce188fe
renato <renato@teste.com>
2024-10-01 00:24:13 -0300

    atividade typescript

---
+++ src/saudacao.js
@@ -0,0 +1,9 @@
+"use strict";
+//funcão para retornar Olá+nome
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+;
+const seuNome = "Renato";
+const saudacaoSeuNome = saudacao(seuNome);
+console.log(saudacaoSeuNome);
